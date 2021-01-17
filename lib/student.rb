06af95d1dd3c8ae9e04b6f1db5786c8f5888a132
David Strauch <dmaster18@gmail.com>
2021-01-17 23:04:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,21 +1,33 @@
-class Student ##student class
+class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url, :scraper
 
   @@all = []
-  
+
   def initialize(student_hash)
-    @name = student_hash[0]  
-    @location = student_hash[1]  
-    @profile_url = student_hash[2]
+    #binding.pry
+    @name = student_hash[:name]
+    @location = student_hash[:location]
+    student_hash = self
+    @@all << self
+
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student|
+      student = self.new(student)
+    end
+
   end
 
   def add_student_attributes(attributes_hash)
-    
+    #binding.pry
+    @twitter = attributes_hash[:twitter]
+    @linkedin = attributes_hash[:linkedin]
+    @github = attributes_hash[:github]
+    @blog = attributes_hash[:blog]
+    @profile_quote = attributes_hash[:profile_quote]
+    @bio = attributes_hash[:bio]
   end
 
   def self.all
