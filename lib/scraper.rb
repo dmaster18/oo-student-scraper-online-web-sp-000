2f957b3be910a6628b078bdbb0c1a862d3c6b526
David Strauch <dmaster18@gmail.com>
2021-01-17 22:56:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     student = doc.css(".main-wrapper")
-    student.map {|student|
+    created_student = student.map {|student|
       {:twitter => student.css('.social-icon-container a')[0].attributes['href'].value, :linkedin => student.css('.social-icon-container a')[1].attributes['href'].value, 
         :github => student.css('.social-icon-container a')[2].attributes['href'].value, :blog=>"http://flatironschool.com", 
         :profile_quote=> student.css(".profile-quote").children.text, :bio=> student.css('.description-holder').children.text
