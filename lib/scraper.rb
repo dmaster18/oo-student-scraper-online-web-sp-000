d3bc350cd67b27c38e74a608e78e56a2f9805814
David Strauch <dmaster18@gmail.com>
2021-01-17 23:03:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,30 +1,50 @@
-require 'nokogiri'
+  
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
+  attr_accessor :student
+  @@all = []
 
   def self.scrape_index_page(index_url)
-    doc = Nokogiri::HTML(open(index_url))
-    students = doc.css(".student-card") 
-    created_students = students.map {|student| 
-      {profile_url: student.css("a").first.attributes["href"].value, name: student.css(".student-name").children.text, location: student.css(".student-location").children.text}
-    }
+    html = open(index_url)
+
+    page = Nokogiri::HTML(html)
+
+    students = []
+
+    page.css("div.student-card").each do |student|
+
+      students << {
+         :name => student.css("h4.student-name").text,
+         :location => student.css("p.student-location").text,
+         :profile_url => student.children[1].attributes["href"].value
+      }
+    end
+    students
   end
- 
 
   def self.scrape_profile_page(profile_url)
-    doc = Nokogiri::HTML(open(profile_url))
-    student = doc.css(".main-wrapper")
-    created_student = student.map {|student|
-      {:twitter => student.css('.social-icon-container a')[0].attributes['href'].value, :linkedin => student.css('.social-icon-container a')[1].attributes['href'].value, 
-        :github => student.css('.social-icon-container a')[2].attributes['href'].value, :blog=>"http://joemburgess.com/", 
-        :profile_quote=> student.css(".profile-quote").children.text, :bio=> student.css('.description-holder').children.text.strip
-      }
-    }
-    created_student[0]
+    page = Nokogiri::HTML(open(profile_url))
+
+    student_page = {}
+
+    social_links = page.css(".social-icon-container").css('a').collect {|e| e.attributes["href"].value}
+
+    social_links.detect do |e|
+
+      student_page[:twitter] = e if e.include?("twitter")
+      student_page[:linkedin] = e if e.include?("linkedin")
+      student_page[:github] = e if e.include?("github")
+
+    end
+
+    student_page[:blog] = social_links[3] if social_links[3] != nil
+    student_page[:profile_quote] = page.css(".profile-quote")[0].text
+    student_page[:bio] = page.css(".description-holder").css('p')[0].text
+    student_page
   end
 
+
 end
-
-
